f0fcb752f8b9d8a78bb258e21d4130569215a919
Nicholas <Nicholasshamshum@gmail.com>
2024-09-05 15:18:08 +0200

    Removed nested if statments and made all else if, seems to cover a better variety of conditions

--- calculate_avg_score/calculate_avg_score.ts
+++ calculate_avg_score/calculate_avg_score.ts
@@ -7,21 +7,21 @@ const CalculateAvgScore = (teamJohnScore: number, teamMarkScore: number, teamMer
 
 	if (teamMarkScore > teamJohnScore && teamMarkScore > teamMerryScore) {
 		console.log(`Mark's Team won with the avg score of: ${teamMarkScore}`);
-		if (teamJohnScore === teamMerryScore) {
-			console.log(`John and Merry drew with the avg score of: ${teamJohnScore}`);
-		}
+
+	} else if (teamJohnScore === teamMerryScore) {
+		console.log(`John and Merry drew with the avg score of: ${teamJohnScore}`);
 
 	} else if (teamMerryScore > teamMarkScore && teamMerryScore > teamJohnScore) {
 		console.log(`Merry's Team won with the avg score of: ${teamMerryScore}`);
-		if (teamJohnScore === teamMarkScore) {
-			console.log(`John and Mark drew with the avg score of: ${teamJohnScore}`);
-		}
+
+	} else if (teamJohnScore === teamMarkScore) {
+		console.log(`John and Mark drew with the avg score of: ${teamJohnScore}`);
 
 	} else if (teamJohnScore > teamMarkScore && teamJohnScore > teamMerryScore) {
 		console.log(`John's Team won with the avg score of: ${teamJohnScore}`);
-		if (teamMarkScore === teamMerryScore) {
-			console.log(`Mark and Merry drew with the avg score of: ${teamMarkScore}`);
-		}
+
+	} else if (teamMarkScore === teamMerryScore) {
+		console.log(`Mark and Merry drew with the avg score of: ${teamMarkScore}`);
 
 	} else {
 		console.log(`Mark's Team, Merry's Team and John's Team drew with the avg score of: ${teamMarkScore} \nMark's Team: ${teamMarkScore} \nJohn's Team: ${teamJohnScore} \nMerry's Team: ${teamMerryScore}`);
@@ -30,8 +30,8 @@ const CalculateAvgScore = (teamJohnScore: number, teamMarkScore: number, teamMer
 	return highestScore;
 }
 
-const johnScore = (100 + 100 + 100) / 3;
-const markScore = (420 + 100 + 100) / 3;
-const merryScore = (302 + 100 + 100) / 3;
+const johnScore = parseFloat(((115 + 190 + 100) / 3).toFixed(2));
+const markScore = parseFloat(((250 + 120 + 40) / 3).toFixed(2));
+const merryScore = parseFloat(((250 + 120 + 40) / 3).toFixed(2));
 
 console.log("The highest average score is: " + CalculateAvgScore(johnScore, markScore, merryScore));
